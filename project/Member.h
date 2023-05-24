791359a004520f22df59d05b111d911dbc06c4c7
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-05-24 19:05:22 +0900

    Member.h

--- project/Member.h
+++ project/Member.h
@@ -12,19 +12,22 @@ class Member {
         string password;
         string address;
         string phoneNumber;
+        int memberType; // 1 - 회사회원, 2 - 일반회원
 
     public:
         Member(string id, string password);
-        void createMember(vector<Member*>& memberList);
+
+        Member createMember(FILE * output_File, string name, string number, int memberType, vector<Member*>& memberList);
         bool verifyMember(vector<Member*>& memberList);
-        void activateMember(vector<Member*>& memberList);
-        void deactivateMember(Member* member, vector<Member*>& memberList);
-        void deleteMember(Member* member, vector<Member*>& memberList);
+        Member activateMember(FILE * output_File, vector<Member*>& memberList);
+        void deactivateMember(FILE * output_File, Member* member, vector<Member*>& memberList);
+        void deleteMember(FILE * output_File, Member* member, vector<Member*>& memberList);
 
         string getId();
         string getPassword();
         string getAddress();
         string getPhoneNumber();
+        int getType();
 };
 
 #endif
